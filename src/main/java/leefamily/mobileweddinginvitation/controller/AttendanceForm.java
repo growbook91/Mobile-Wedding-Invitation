600b3da01388956ddfde29b535a1009a7faf70ef
kangmin lee <growbook91@gmail.com>
2023-10-31 17:17:08 +0900

    Attendance Form is created

---
+++ src/main/java/leefamily/mobileweddinginvitation/controller/AttendanceForm.java
@@ -0,0 +1,59 @@
+package leefamily.mobileweddinginvitation.controller;
+
+public class AttendanceForm {
+    private Long id;
+    private String side;
+    private String name;
+    private String companionName;
+    private String food;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSide() {
+        return side;
+    }
+
+    public void setSide(String side) {
+        this.side = side;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCompanionName() {
+        return companionName;
+    }
+
+    public void setCompanionName(String companionName) {
+        this.companionName = companionName;
+    }
+
+    public String getFood() {
+        return food;
+    }
+
+    public void setFood(String food) {
+        this.food = food;
+    }
+
+    public Integer getTotalNum() {
+        return totalNum;
+    }
+
+    public void setTotalNum(Integer totalNum) {
+        this.totalNum = totalNum;
+    }
+
+    private Integer totalNum;
+}
