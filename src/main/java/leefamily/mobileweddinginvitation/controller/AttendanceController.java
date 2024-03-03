870d29aa2a406381cb8d6aa38ee97f0c1291daf0
growbook91 <70497021+growbook91@users.noreply.github.com>
2024-03-04 00:34:26 +0900

    참석 여부에 대한 쿠키 추가

--- src/main/java/leefamily/mobileweddinginvitation/controller/AttendanceController.java
+++ src/main/java/leefamily/mobileweddinginvitation/controller/AttendanceController.java
@@ -7,6 +7,10 @@ import leefamily.mobileweddinginvitation.service.CommentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class AttendanceController {
@@ -31,7 +35,7 @@ public class AttendanceController {
 
     // 참석 여부 조사
     @PostMapping(value = "/attendance/new")
-    public String create(AttendanceForm form) {
+    public String create(AttendanceForm form, HttpServletResponse response) {
         //AttendanceForm을 그냥 attendance로 해도 될 것 같은데 나중에 수정하자.
         Attendance attendance = new Attendance();
         attendance.setSide(form.getSide());
@@ -41,8 +45,21 @@ public class AttendanceController {
         attendance.setFood(form.getFood());
         attendanceService.writeAttendance(attendance);
 
+        // 참석에 대한 쿠키 생성
+        Cookie cookie = new Cookie("popupYN", "N");
+        cookie.setDomain("kangminlovesjihye.ddns.net");
+        cookie.setPath("/");
+        // 30초간 저장
+        cookie.setMaxAge(5*60);
+        response.addCookie(cookie);
+
+        //blank로 가면 무슨 의미..?
+        //blank.html로 가는 거구나
+        //blank.html에서는 팝업 창을 닫고 새로고침.
+        //TODO : 이건 나중에 큰 개선이 필요할 것 같다.
         return "blank";
     }
+
 
     // 여기에 validation하는 함수를 하나 더 생성한다.
     // 그래서 통과하면 블랭크 아니면 attendanc/new로 다시 돌아간다.
